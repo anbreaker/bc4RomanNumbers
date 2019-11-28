00ad710dbe6eb27756872fd3dd2e4fe247c592f3
anbreaker <antunez19@gmail.com>
2019-11-28 19:54:36 +0100

    test decrecientes... resultado en negativo -99 != 99

--- romanNumbers.py
+++ romanNumbers.py
@@ -4,28 +4,36 @@
 
 # valoresNumerosRomanos = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
 
-valores = {'M': 1000, 'CM': 900, 'D': 500, 'C': 100,
-           'XC': 90, 'L': 50, 'X': 10, 'IX': 9, 'V': 5, 'I': 1}
+valores = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
 
-def romano_a_arabigo(numRomano):
+def romano_a_arabigo(numRomano):  # (XCIX) -> 99
     numArabigo = 0
     numRepes = 1
     ultimoCaracter = ''
+
     for letra in numRomano:
         if letra == ultimoCaracter:
             numRepes += 1
         else:
             numRepes = 1
-
         if numRepes > 3:
             return 0
 
         if letra in valores:
-            numArabigo += valores[letra]
+            if ultimoCaracter == '':
+                numArabigo += valores.get(letra)
+                pass
+            elif valores.get(letra) <= valores.get(ultimoCaracter):
+                numArabigo += valores.get(letra)
+            else:
+                numArabigo -= valores.get(letra)
         else:
             return 0
 
         ultimoCaracter = letra
 
     return numArabigo
+
+
+ver = print(romano_a_arabigo('XCIX'))
--- testRomanNumbers.py
+++ testRomanNumbers.py
@@ -15,18 +15,23 @@ class RomanNumberTest(unittest.TestCase):
         self.assertEqual(romano_a_arabigo('A'), 0)
 
     def test_numeros_crecientes(self):
-        self.assertEqual(romano_a_arabigo('XVI'), 16)
         self.assertEqual(romano_a_arabigo('III'), 3)
-
-        #self.assertEqual(romano_a_arabigo('IIII', 0))
+        self.assertEqual(romano_a_arabigo('IIII'), 0)
+        self.assertEqual(romano_a_arabigo('XVI'), 16)
+        self.assertEqual(romano_a_arabigo('XXIII'), 23)
+        self.assertEqual(romano_a_arabigo('CCC'), 300)
 
     def test_no_mas_de_tres_repeticiones(self):
         self.assertEqual(romano_a_arabigo('LXXIII'), 73)
         self.assertEqual(romano_a_arabigo('IIII'), 0)
+        self.assertEqual(romano_a_arabigo('CCCC'), 0)
 
     def test_numeros_decrecientes(self):
+        self.assertEqual(romano_a_arabigo('XCIX'), 99)
         self.assertEqual(romano_a_arabigo('CMXCIX'), 999)
+        self.assertEqual(romano_a_arabigo('MMCMLXIX'), 2969)
         self.assertEqual(romano_a_arabigo('VC'), 0)
+        self.assertEqual(romano_a_arabigo('MVL'), 0)
 
 
 if __name__ == '__main__':
